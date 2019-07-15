3b9f5774ff7b7ec4dace1104093d91d5bbb80420
kevinqijie <15131547031@163.com>
2019-07-16 07:00:45 +0800

    g1

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,27 @@
+
 module.exports = {
-    baseUrl: './',
-    productionSourceMap: false,
+  publicPath: '/', //基本路径
+  outputDir: 'dist',
+  productionSourceMap: false,
+  assetsDir: 'static',
+  filenameHashing: true,
+  pages: {
+    index: {
+      // page 的入口
+      entry: "src/main.js",
+      // 模板来源
+      template: "public/index.html", // 这里用来区分加载那个 html
+      // 在 dist/index.html 的输出
+      filename: "index.html",
+      // 在这个页面中包含的块，默认情况下会包含
+      // 提取出来的通用 chunk 和 vendor chunk。
+      chunks: ["chunk-vendors", "chunk-common", "index"]
+    }
+},
+
     devServer: {
         compress: true,
-       
-        disableHostCheck: true,   // That solved it
+       disableHostCheck: true,   // That solved it
         open:true,
         proxy: {
             '/api':{
