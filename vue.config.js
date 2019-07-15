6569e1bcb028ed495c2742c9c3a4e3ff2e0f7e3a
kevinqijie <15131547031@163.com>
2019-07-16 07:18:23 +0800

    g4

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,22 @@
+const path = require('path');
+ 
+// function resolve(dir) {
+//   return path.join(__dirname, dir)
+// }
+ 
+// 导入compression-webpack-plugin
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+// const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+// const externals = {
+//   'vue': 'Vue',
+//   'vue-router': 'VueRouter',
+//   'vuex': 'Vuex',
+//   'axios': 'axios',
+//   'ELEMENT': 'element-ui'
+// }
+// 定义压缩文件类型
+const productionGzipExtensions = ['js', 'css']
+
 
 module.exports = {
   publicPath: '/', //基本路径
@@ -18,7 +37,40 @@ module.exports = {
       chunks: ["chunk-vendors", "chunk-common", "index"]
     }
 },
-
+configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'test') {
+      // config.externals = externals
+      config.plugins.push(
+        new CompressionWebpackPlugin({
+          filename: '[path].gz[query]',
+          algorithm: 'gzip',
+          test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+          threshold: 10240,
+          minRatio: 0.8
+        }),
+      //   new UglifyJsPlugin({
+      //     uglifyOptions: {
+      //       compress: {
+      //         warnings: false,
+      //         drop_debugger: true,
+      //         drop_console: true,
+      //       },
+      //     },
+      //     sourceMap: false,
+      //     parallel: true,
+      //   }),
+      );
+      const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+      config.plugins.push(new BundleAnalyzerPlugin());
+    }
+  },
+  // CSS 相关选项
+  css: {
+    extract: true,
+    sourceMap: false,
+    loaderOptions: {}, // 为所有的 CSS 及其预处理文件开启 CSS Modules。
+    modules: false
+  },
     devServer: {
         compress: true,
        disableHostCheck: true,   // That solved it
